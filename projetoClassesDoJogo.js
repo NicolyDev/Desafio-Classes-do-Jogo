71a2b2e9fa2cd398a5d459c7639e97e31d5cffc5
NicolyDev <156141652+NicolyDev@users.noreply.github.com>
2024-02-16 16:48:03 -0300

    Update projetoClassesDoJogo.js
    Consertei alguns erros de comparação que tinha no if/else;
    encapsulei o if/else o tipoDeAtaque dentro de uma função na classe chamada definirTipoDeAtaque;
    coloquei o ponto e vírgula nas linhas de código em que se podia estar colocando, para organização do código.

--- projetoClassesDoJogo.js
+++ projetoClassesDoJogo.js
@@ -1,29 +1,27 @@
 class heroi{
   constructor(nome, idade, tipo, tipoDeAtaque){
-    this.nome = nome
-    this.idade = idade
-    this.tipo = tipo
-    this.tipoDeAtaque = tipoDeAtaque
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+    this.tipoDeAtaque = this.definirTipoDeAtaque();
   }
   //usar if/else pra atribuir o valor de ataque conforme o tipo de heroi
- 
+  definirTipoDeAtaque(){
+    if(this.tipo === 'mago'){
+     return 'magia';
+    }else if(this.tipo === 'monge'){
+     return 'artes marciais';
+    }else if(this.tipo === 'ninja'){
+     return 'shuriken';
+    }else{
+     return 'espada';
+    }
+  }
+  
   ataque(){
-     console.log(`Herói de nome ${this.nome}, de ${this.idade} anos, ${this.tipo}, atacou usando ${this.tipoDeAtaque}`)
+     console.log(`Herói de nome ${this.nome}, de ${this.idade} anos, ${this.tipo}, atacou usando ${this.tipoDeAtaque}`);
  }
 }
 
-
-let heroi1 = new heroi('Thartra', 18, 'mago')
-
-if(heroi1.tipo = 'mago'){
-   heroi1.tipoDeAtaque = 'magia' 
- }else if(heroi1.tipo = 'monge'){
-   heroi1.tipoDeAtaque = 'artes marciais'
- }else if(heroi1.tipo = 'ninja'){
-  heroi1.tipoDeAtaque(heroi1.tipo = 'shuriken')
- }else{
-  heroi1.tipoDeAtaque = 'espada'
- }
- 
-
-heroi1.ataque()
+let heroi1 = new heroi('Thartra', 18, 'mago');
+heroi1.ataque();
